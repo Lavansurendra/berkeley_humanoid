810d7ab335a6045dc2f799e39f587e8f6f7d77aa
unknown <lavansurendra@gmail.com>
2026-03-19 19:33:19 -0500

    fixing minor bugs

--- exts/berkeley_humanoid/berkeley_humanoid/tasks/locomotion/__init__.py
+++ exts/berkeley_humanoid/berkeley_humanoid/tasks/locomotion/__init__.py
@@ -1,8 +1,8 @@
-# Copyright (c) 2022-2024, The Berkeley Humanoid Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
+# # Copyright (c) 2022-2024, The Berkeley Humanoid Project Developers.
+# # All rights reserved.
+# #
+# # SPDX-License-Identifier: BSD-3-Clause
 
-"""Locomotion environments for legged robots."""
+# """Locomotion environments for legged robots."""
 
-from .velocity import *  # noqa
+# from .velocity import *  # noqa
