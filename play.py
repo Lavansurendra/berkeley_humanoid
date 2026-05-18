0f3994bb57d810a500cbc8e33b44034e0fa9b075
RMiranda4 <robertmiranda1604@gmail.com>
2026-05-18 01:30:03 -0500

    THIS IS THE NEW BEST VERSION. IMPORTANT CHANGE: got rid of linear scheduling for learning rate and greatly increased learning rate. IMPORTANT CHANGE: fixed bug in gait starting -> changed condition from elif to if. IMPORTANT CHANGE: changed push to beign walking motion to be much more gradual and follow a sinusoid. IMPORTANT CHANGE: increased foot tortional and sliding friction. IMPORTANT CHANGE: changed crouch nominal position to have pelvis a bit higher and legs a bit straighter to prevent pushing off the floor and bouncing at beginning of gait due to bent legs and soft contact dynamics as foot goes through the floor

--- play.py
+++ play.py
@@ -14,10 +14,10 @@ def main():
     
     # 2. Update model paths to point to the new output/models directory
     models_dir = "output/models"
-    model_name = "g1_final_3160000"
+    model_name = "g1_final_400000"
     
     model_path = os.path.join(models_dir, model_name) # No .zip needed for PPO.load
-    stats_path = os.path.join(models_dir, "g1_vecnormalize_3160000.pkl")
+    stats_path = os.path.join(models_dir, "g1_vecnormalize_400000.pkl")
     
     print(f"[INFO] Loading MuJoCo Environment...")
     
